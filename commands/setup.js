3637edbbf532aa908e725d4868a2e97d8b1fcfb9
jacob-tucker <jacobtucker818@gmail.com>
2021-12-17 23:46:58 -0500

    is nan check

--- commands/setup.js
+++ commands/setup.js
@@ -2,12 +2,11 @@ const { changeAuthData } = require('../flowscripts/write_data.js');
 const { Permissions } = require('discord.js');
 
 const execute = (message, args) => {
-    if (message.member.permissions.has(Permissions.FLAGS.MANAGE_GUILD) && (args.length === 4 || args.length === 5) && (typeof args[1] === 'number')) {
+    if (message.member.permissions.has(Permissions.FLAGS.MANAGE_GUILD) && (args.length === 4 || args.length === 5) && (!isNaN(args[1]))) {
         let role = message.guild.roles.cache.find(role => role.name === args[3]);
         // GuildID, NFT/FT, #, public path name, role name, optional minting link
         if (args.length === 4) changeAuthData(message.guild.id, args[0], args[1], args[2], role.id, "")
         else if (args.length === 5) changeAuthData(message.guild.id, args[0], args[1], args[2], role.id, args[4])
-        console.log("Has permissions.")
     } else if (message.member.permissions.has(Permissions.FLAGS.MANAGE_GUILD)) {
         message.channel.send("You did not supply the correct number of arguments. `!setup [NFT/FT] [number of tokens] [public path] [role name] [OPTIONAL: link to the minting site]`")
     } else {
