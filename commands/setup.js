c3e63fa4e3ccba3e1de48ad4ddad4c72eaf1b64c
jacob-tucker <jacobtucker818@gmail.com>
2021-12-17 23:11:50 -0500

    fix block

--- commands/setup.js
+++ commands/setup.js
@@ -4,9 +4,9 @@ const { Permissions } = require('discord.js');
 const execute = (message, args) => {
     if (message.member.permissions.has(Permissions.FLAGS.MANAGE_GUILD) && (args.length === 4 || args.length === 5)) {
         let role = message.guild.roles.cache.find(role => role.name === args[3]);
-        // 1. GuildID, NFT/FT, #, public path name, role name, optional minting link
-        if (args.length === 3) changeAuthData(message.guild.id, args[0], args[1], args[2], role.id, "")
-        else if (args.length === 4) changeAuthData(message.guild.id, args[0], args[1], args[2], role.id, args[4])
+        // GuildID, NFT/FT, #, public path name, role name, optional minting link
+        if (args.length === 4) changeAuthData(message.guild.id, args[0], args[1], args[2], role.id, "")
+        else if (args.length === 5) changeAuthData(message.guild.id, args[0], args[1], args[2], role.id, args[4])
         console.log("Has permissions.")
     } else if (message.member.permissions.has(Permissions.FLAGS.MANAGE_GUILD)) {
         message.channel.send("You did not supply the correct number of arguments. `!setup [NFT/FT] [number of tokens] [public path] [role name] [OPTIONAL: link to the minting site]`")
