6f70d71e25410060d1fccd95b444eb2a07eda1f5
jacob-tucker <jacobtucker818@gmail.com>
2021-12-27 16:38:14 -0500

    key

--- helperfunctions/functions.js
+++ helperfunctions/functions.js
@@ -1,6 +1,6 @@
 const crypto = require('crypto');
 const algorithm = 'aes-256-cbc'; //Using AES encryption
-const key = crypto.randomBytes(32);
+const key = process.env.CRYPTO_KEY;
 const iv = crypto.randomBytes(16);
 
 // Encrypting text
