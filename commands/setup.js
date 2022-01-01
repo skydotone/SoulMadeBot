e322a48226fc18e7cb0f6f36c4fd28400c523661
jacob-tucker <jacobtucker818@gmail.com>
2021-12-31 19:41:52 -0500

    find update

--- commands/setup.js
+++ commands/setup.js
@@ -1,19 +1,31 @@
 const { changeAuthData } = require('../flowscripts/write_data.js');
 const { Permissions } = require('discord.js');
-
+/*
+    args[0] == NFT/FT/FIND
+    args[1] == contract name
+    args[2] == contract address
+    args[3] == # of token
+    args[4] == public path
+    args[5] == discord role
+    args[6] == mainnet/testnet
+    args[7] == optional minting link
+*/
 const execute = (message, args) => {
-    if (message.member.permissions.has(Permissions.FLAGS.MANAGE_GUILD) && (args.length === 7 || args.length === 8) && (!isNaN(args[3])) && (args[0] === 'NFT' || args[0] === 'FT') && (args[6] === "mainnet" || args[6] === "testnet")) {
-        let role = message.guild.roles.cache.find(role => role.name === args[5]);
-        if (!role) {
-            message.channel.send("This role does not exist!");
-            return;
-        }
+    if (message.member.permissions.has(Permissions.FLAGS.MANAGE_GUILD)) {
+        if ((args.length === 7 || args.length === 8) && (!isNaN(args[3])) && (args[0] === 'NFT' || args[0] === 'FT') && (args[6] === "mainnet" || args[6] === "testnet")) {
+            let role = message.guild.roles.cache.find(role => role.name === args[5]);
+            if (!role) {
+                message.channel.send("This role does not exist!");
+                return;
+            }
 
-        // GuildID, NFT/FT, #, public path name, role id, optional minting link, network
-        if (args.length === 7) changeAuthData(message.guild.id, args[0], args[1], args[2], args[3], args[4], role.id, "", args[6])
-        else if (args.length === 8) changeAuthData(message.guild.id, args[0], args[1], args[2], args[3], args[4], role.id, args[7], args[6])
-    } else if (message.member.permissions.has(Permissions.FLAGS.MANAGE_GUILD)) {
-        message.channel.send("You did not supply the correct number of arguments. `!setup [NFT/FT] [number of tokens] [public path] [role name] [OPTIONAL: link to the minting site]`")
+            if (args.length === 7) changeAuthData(message.guild.id, args[0], args[1], args[2], args[3], args[4], role.id, "", args[6])
+            else if (args.length === 8) changeAuthData(message.guild.id, args[0], args[1], args[2], args[3], args[4], role.id, args[7], args[6])
+        } else if (args.length == 2 && args[0] === 'FIND') {
+            changeAuthData(message.guild.id, args[0], "", "", 1, "", args[1], "https://find.xyz", "mainnet")
+        } else {
+            message.channel.send("You did not supply the correct number of arguments. `!setup [NFT/FT] [number of tokens] [public path] [role name] [OPTIONAL: link to the minting site]`")
+        }
     } else {
         console.log("You do not have permissions to do this.")
     }
--- flowscripts/check_token.js
+++ flowscripts/check_token.js
@@ -66,6 +66,50 @@ const getBalance = async (AccountProof, guildID, network) => {
         return -1
       }
     `;
+  } else if (tokenType === "FIND") {
+    script = `
+      import FIND from 0x097bafa4e0b48eef
+      import Profile from 0x097bafa4e0b48eef
+
+      pub fun main(address: Address): Int {
+
+        let account=getAccount(address)
+        let leaseCap = account.getCapability<&FIND.LeaseCollection{FIND.LeaseCollectionPublic}>(FIND.LeasePublicPath)
+
+        if !leaseCap.check() {
+          return 0
+        }
+
+        let profile= Profile.find(address).asProfile()
+        let leases = leaseCap.borrow()!.getLeaseInformation() 
+        var time : UFix64?= nil
+        var name :String?= nil
+        for lease in leases {
+
+          //filter out all leases that are FREE or LOCKED since they are not actice
+          if lease.status != "TAKEN" {
+            continue
+          }
+
+          //if we have not set a 
+          if profile.findName == "" {
+            if time == nil || lease.validUntil < time! {
+              time=lease.validUntil
+              name=lease.name
+            }
+          }
+
+          if profile.findName == lease.name {
+            return 1
+          }
+        }
+
+        if name == nil {
+          return 0
+        } 
+        return 1
+      }
+    `
   }
 
   const result = await fcl.send([
