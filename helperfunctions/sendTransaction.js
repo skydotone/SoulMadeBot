3e59d2d10132244b6ff06e7ca677ddd60aaafa00
Jacob Tucker <tsnakejake@aol.com>
2022-02-06 10:15:56 -0600

    back up

--- helperfunctions/sendTransaction.js
+++ helperfunctions/sendTransaction.js
@@ -5,16 +5,21 @@ const {authorizationFunctionProposer, authorizationFunction} = require("./author
 
 const sendTransaction = async (code, args) => {
     await setEnvironment("testnet");
-    const transactionId = await fcl.send([
-        fcl.transaction(code),
-        fcl.args(args),
-        fcl.proposer(authorizationFunctionProposer),
-        fcl.payer(authorizationFunction),
-        fcl.authorizations([authorizationFunction]),
-        fcl.limit(9999)
-    ]).then(fcl.decode);
 
-    return transactionId;
+    try {
+        const transactionId = await fcl.send([
+            fcl.transaction(code),
+            fcl.args(args),
+            fcl.proposer(authorizationFunctionProposer),
+            fcl.payer(authorizationFunction),
+            fcl.authorizations([authorizationFunction]),
+            fcl.limit(9999)
+        ]).then(fcl.decode);
+    
+        return transactionId;
+    } catch(e) {
+        console.log(e);
+    }
 }
 
 module.exports = {
