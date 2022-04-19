aee32cc443864d91bdd0d2c9607dfaa3eea5e512
Jacob Tucker <tsnakejake@aol.com>
2022-04-19 15:35:28 -0500

    add csvmaker func

--- commands/userswithrole.js
+++ commands/userswithrole.js
@@ -37,6 +37,30 @@ const sendInfo = async (interaction, role) => {
   await interaction.editReply({content: `Users with the <@&${role.id}> role:`, files: [userList]});
 }
 
+const csvmaker = function (data) {
+ 
+  // Empty array for storing the values
+  csvRows = [];
+
+  // Headers is basically a keys of an
+  // object which is id, name, and
+  // profession
+  const headers = Object.keys(data);
+
+  // As for making csv format, headers
+  // must be separated by comma and
+  // pushing it into array
+  csvRows.push(headers.join(','));
+
+  // Pushing Object values into array
+  // with comma separation
+  const values = Object.values(data).join(',');
+  csvRows.push(values)
+
+  // Returning the array joining with new line
+  return csvRows.join('\n')
+}
+
 module.exports = {
   name: 'userswithrole',
   description: 'get discord name and address of someone in a role',
