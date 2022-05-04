b6bde208f7cd82fbd82ce5e620d7bfd4bf749ae8
Jacob Tucker <tsnakejake@aol.com>
2022-05-03 23:09:12 -0500

    toFixed in ft script

--- flow/scripts/checkOwnsFT.js
+++ flow/scripts/checkOwnsFT.js
@@ -2,16 +2,17 @@ const fcl = require('@onflow/fcl');
 const t = require('@onflow/types');
 
 const checkOwnsFT = async (contractName, contractAddress, publicPath, amount, user) => {
+  const fixedAmount = amount.toFixed(2);
   const scriptCode = `
   import FungibleToken from 0xf233dcee88fe0abe
   import ${contractName} from ${contractAddress}
   pub fun main(address: Address): Bool {
       if let vault = getAccount(address).getCapability(/public/${publicPath}).borrow<&${contractName}.Vault{FungibleToken.Balance}>() {
-          return vault.balance >= ${amount}
+          return vault.balance >= ${fixedAmount}
       } else {
           if let vaultPublic = getAccount(address).getCapability(/public/${publicPath}).borrow<&{FungibleToken.Balance}>() {
               if vaultPublic.getType().identifier == "A.${contractAddress.slice(2)}.${contractName}.Vault" {
-                  return vaultPublic.balance >= ${amount}
+                  return vaultPublic.balance >= ${fixedAmount}
               }
           }
       }
