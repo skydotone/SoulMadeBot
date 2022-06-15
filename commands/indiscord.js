ad669ca41943a2ea55cafc292d5297371eafdba1
jacob-tucker <jacobtucker818@gmail.com>
2022-06-14 23:00:01 -0400

    try

--- commands/indiscord.js
+++ commands/indiscord.js
@@ -4,9 +4,7 @@ const { MessageAttachment, Permissions } = require('discord.js');
 const execute = async (interaction, options) => {
   if (interaction.member.permissions.has(Permissions.FLAGS.MANAGE_GUILD)) {
     await interaction.deferReply({ ephemeral: true });
-    const channel = interaction.member.voiceChannel;
-    console.log(interaction.member)
-    console.log(interaction)
+    console.log('INTERACTION CHANNEL', interaction.channel)
     const membersInChannel = channel.members.map(member => member.id);
 
     sendInfo(interaction, membersInChannel);
